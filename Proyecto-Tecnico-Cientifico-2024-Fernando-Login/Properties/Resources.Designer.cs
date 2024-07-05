15511817d9be9d5047393136848a1537e6857e0c
Fernando Velasquez <fernadomiguelvelasquezperez@gmail.com>
2024-07-05 15:03:52 -0600

    Actualizacion de login y recuperar contraseña

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Properties/Resources.Designer.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace PTC2024.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap User {
+            get {
+                object obj = ResourceManager.GetObject("User", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
