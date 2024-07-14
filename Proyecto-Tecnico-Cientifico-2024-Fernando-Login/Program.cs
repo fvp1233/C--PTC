e4fb83a1e246d8658a4b347ed93c8c343c36fa73
Fernando Velasquez <fernadomiguelvelasquezperez@gmail.com>
2024-07-14 16:12:49 -0600

    Se creo el formulario register con todos sus campos de vista

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Program.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Program.cs
@@ -7,6 +7,7 @@ using PTC2024.formularios.login;
 using PTC2024.View.Empleados;
 using PTC2024.View.Facturacion;
 using PTC2024.View.formularios.inicio;
+using PTC2024.View.login;
 
 namespace PTC2024
 {
@@ -20,7 +21,7 @@ namespace PTC2024
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmAddEmployee());
+            Application.Run(new FrmRegister());
         }
     }
 }
