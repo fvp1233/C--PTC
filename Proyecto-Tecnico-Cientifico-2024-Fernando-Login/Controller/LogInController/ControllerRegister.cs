651c5e3772432632e38a33785274da1bbf41b87a
Fernando Velasquez <fernadomiguelvelasquezperez@gmail.com>
2024-07-18 15:30:03 -0600

    Cambios en Controller y DTO Register

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Controller/LogInController/ControllerRegister.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Controller/LogInController/ControllerRegister.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using PTC2024.Model.DAO.LogInDAO;
+using PTC2024.View.formularios.inicio;
 using PTC2024.View.login;
 
 namespace PTC2024.Controller.LogInController
@@ -13,34 +14,39 @@ namespace PTC2024.Controller.LogInController
     internal class ControllerRegister
     {
         FrmRegister objNewUser;
-        public ControllerRegister (FrmRegister view)
+        public ControllerRegister(FrmRegister view)
         {
             objNewUser = view;
             objNewUser.btnRegister.Click += new EventHandler(RegisterData);
         }
 
-        public void RegisterData (object sender, EventArgs e)
+        public void RegisterData(object sender, EventArgs e)
         {
             DAORegister DAOInsertar = new DAORegister();
 
             //Insercion de datos de tabla tbEmployees
             DAOInsertar.Names = objNewUser.txtNames.Text;
             DAOInsertar.Lastnames = objNewUser.txtLastnames.Text;
-            DAOInsertar.Birth1 = objNewUser.dtBirth.Value.Date;
+            DAOInsertar.Birth = objNewUser.dtBirth.Value.Date;
             DAOInsertar.Email = objNewUser.txtEmail.Text;
             DAOInsertar.DUI1 = objNewUser.txtDUI.Text;
             DAOInsertar.Phone = objNewUser.txtPhone.Text;
             DAOInsertar.Address = objNewUser.txtAddress.Text;
-            DAOInsertar.User = objNewUser.txtUser.Text;
+            DAOInsertar.BusinessP = 1;
+            DAOInsertar.Department = 1;
+            DAOInsertar.TypeE = 1;
+            DAOInsertar.MaritalStatus = "Soltero";
+            DAOInsertar.Status = 1;
 
             //Insercion de datos de tabla tbUserData
-            DAOInsertar.User = objNewUser.txtUser.Text ;
-            DAOInsertar.User = objNewUser.txtConfirmedPassword.Text;
-            int returnn= DAOInsertar.GetNames();
+            DAOInsertar.User = objNewUser.txtUser.Text;
+            DAOInsertar.Password = objNewUser.txtPassword.Text;
+            DAOInsertar.ConfirmPassword = objNewUser.txtConfirmedPassword.Text;
+            int returnn = DAOInsertar.GetNames();
 
             if (returnn == 1) {
-               
-                MessageBox.Show("Datos ingresados correctamente" +  MessageBoxButtons.OK + MessageBoxIcon.Information);
+
+                MessageBox.Show("Datos ingresados correctamente" + MessageBoxButtons.OK + MessageBoxIcon.Information);
 
             }
 
@@ -49,8 +55,15 @@ namespace PTC2024.Controller.LogInController
                 MessageBox.Show("No se pudieron ingresar los datos", "Proceso interrumpido", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
+            //Eventos visuales
+
 
         }
 
-    }
+        
 }
+        
+
+
+    }
+
--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/DTO/LogInDTO/DTORegister.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/DTO/LogInDTO/DTORegister.cs
@@ -11,7 +11,7 @@ namespace PTC2024.Model.DTO.LogInDTO
         //Atributos del formulario Register
         private string names;
         private string lastnames;
-        private DateTime Birth;
+        private DateTime birth;
         private string email;
         private string DUI;
         private string phone;
@@ -19,11 +19,16 @@ namespace PTC2024.Model.DTO.LogInDTO
         private string user;
         private string password;
         private string confirmPassword;
+        private int businessP;
+        private int department;
+        private int typeE;
+        private string maritalStatus;
+        private int status;
 
         //Getter y Settersde los atributos
         public string Names { get => names; set => names = value; }
         public string Lastnames { get => lastnames; set => lastnames = value; }
-        public DateTime Birth1 { get => Birth; set => Birth = value; }
+        
         public string Email { get => email; set => email = value; }
         public string DUI1 { get => DUI; set => DUI = value; }
         public string Phone { get => phone; set => phone = value; }
@@ -31,8 +36,11 @@ namespace PTC2024.Model.DTO.LogInDTO
         public string User { get => user; set => user = value; }
         public string Password { get => password; set => password = value; }
         public string ConfirmPassword { get => confirmPassword; set => confirmPassword = value; }
-
-       
-
+        public int BusinessP { get => businessP; set => businessP = value; }
+        public DateTime Birth { get => birth; set => birth = value; }
+        public int Department { get => department; set => department = value; }
+        public int TypeE { get => typeE; set => typeE = value; }
+        public string MaritalStatus { get => maritalStatus; set => maritalStatus = value; }
+        public int Status { get => status; set => status = value; }
     }
 }
