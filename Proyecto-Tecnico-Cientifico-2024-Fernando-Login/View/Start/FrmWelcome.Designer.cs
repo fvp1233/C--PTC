2f8ed694bc2339d55028d1b2b1f909f7836d2667
Oscar Velasquez <oscar.joyar@gmail.com>
2024-07-18 15:27:07 -0600

    Se crearon los childForms, y se elimino el formulario frmEmployee

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/View/Start/FrmWelcome.Designer.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/View/Start/FrmWelcome.Designer.cs
@@ -32,8 +32,8 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.bunifuPictureBox1 = new Bunifu.UI.WinForms.BunifuPictureBox();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
             this.lblNombreUsuario = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuPictureBox1)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
@@ -85,17 +85,6 @@
             this.tableLayoutPanel2.Size = new System.Drawing.Size(1157, 123);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(212, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(378, 63);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "BIENVENIDO,";
-            // 
             // lblNombreUsuario
             // 
             this.lblNombreUsuario.Anchor = System.Windows.Forms.AnchorStyles.Left;
@@ -107,11 +96,22 @@
             this.lblNombreUsuario.TabIndex = 1;
             this.lblNombreUsuario.Text = ".";
             // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(212, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BIENVENIDO,";
+            // 
             // FrmWelcome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Silver;
+            this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(1163, 687);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "FrmWelcome";
