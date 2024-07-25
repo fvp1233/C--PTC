c9a84c2764b15481ea9bcdb1e83fb9f9104bc073
Oscar Velasquez <oscar.joyar@gmail.com>
2024-07-25 07:51:00 -0600

    Se cambio la consulta

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/DAO/PayrollsDAO/DAOAddPayroll.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/DAO/PayrollsDAO/DAOAddPayroll.cs
@@ -9,20 +9,21 @@ using System.Threading.Tasks;
 using System.Web.UI.WebControls;
 using System.Windows.Forms;
 using PTC2024.View.Empleados;
+using System.Windows.Input;
 
 namespace PTC2024.Model.DAO.PayrollsDAO
 {
     internal class DAOAddPayroll : DTOAddPayroll
     {
-        readonly SqlCommand command = new SqlCommand();
+        readonly SqlCommand comand = new SqlCommand();
 
         public DataSet GetEmployeeName()
         {
             try
             {
-                command.Connection = getConnection();
+                comand.Connection = getConnection();
                 string query = "SELECT CONCAT (Id_Employee,'. ',names,' ',lastName) AS fullEmployeeInfo FROM tbEmployee";
-                SqlCommand cmd = new SqlCommand(query, command.Connection);
+                SqlCommand cmd = new SqlCommand(query, comand.Connection);
                 cmd.ExecuteNonQuery();
 
                 SqlDataAdapter adp = new SqlDataAdapter(cmd);
@@ -39,7 +40,43 @@ namespace PTC2024.Model.DAO.PayrollsDAO
             }
             finally
             {
-                command.Connection.Close();
+                comand.Connection.Close();
+            }
+        }
+        public int AddPayroll()
+        {
+            try
+            {
+                comand.Connection = getConnection();
+                string queryPayroll = "INSERT INTO tbPayroll VALUES (@gross_Pay,@net_Pay,@income,@bonus,@month,@security_Number, @AFP, @ISS, @bank_Account,@Id_Employee)";
+                SqlCommand cmdAddPayroll = new SqlCommand(@queryPayroll, comand.Connection);
+
+                cmdAddPayroll.Parameters.AddWithValue("gross_Pay", GrossPay);
+                cmdAddPayroll.Parameters.AddWithValue("net_Pay", NetPay);
+                cmdAddPayroll.Parameters.AddWithValue("income", Income);
+                cmdAddPayroll.Parameters.AddWithValue("bonus", Bonus);
+                cmdAddPayroll.Parameters.AddWithValue("month", IssueDate);
+                cmdAddPayroll.Parameters.AddWithValue("security_Number", SecurityNumber);
+                cmdAddPayroll.Parameters.AddWithValue("AFP", Afp);
+                cmdAddPayroll.Parameters.AddWithValue("ISS", Iss);
+                cmdAddPayroll.Parameters.AddWithValue("bank_Account", BanckAccount);
+                cmdAddPayroll.Parameters.AddWithValue("Id_Employee", IdPayroll);
+
+                int answer = cmdAddPayroll.ExecuteNonQuery();
+                if (answer == 1)
+                {
+                    return 1;
+                }
+                else { return 0; }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show($"EC-009: {ex.Message}", "Error Critico", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return -1;
+            }
+            finally
+            {
+                comand.Connection.Close();
             }
         }
     }
