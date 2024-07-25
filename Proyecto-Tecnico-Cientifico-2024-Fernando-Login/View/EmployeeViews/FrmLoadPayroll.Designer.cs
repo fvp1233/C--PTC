c51924900dd50c42ce0877a44c43427792a52507
Oscar Velasquez <oscar.joyar@gmail.com>
2024-07-25 15:39:27 -0600

    Se agrego el formulario progressBar

---
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/View/EmployeeViews/FrmLoadPayroll.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PTC2024.View.EmployeeViews
+{
+    partial class FrmLoadPayroll
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLoadPayroll));
+            this.bunifuProgressBar1 = new Bunifu.UI.WinForms.BunifuProgressBar();
+            this.bunifuLabel1 = new Bunifu.UI.WinForms.BunifuLabel();
+            this.SuspendLayout();
+            // 
+            // bunifuProgressBar1
+            // 
+            this.bunifuProgressBar1.AllowAnimations = false;
+            this.bunifuProgressBar1.Animation = 0;
+            this.bunifuProgressBar1.AnimationSpeed = 220;
+            this.bunifuProgressBar1.AnimationStep = 10;
+            this.bunifuProgressBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(223)))), ((int)(((byte)(223)))));
+            this.bunifuProgressBar1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuProgressBar1.BackgroundImage")));
+            this.bunifuProgressBar1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(223)))), ((int)(((byte)(223)))));
+            this.bunifuProgressBar1.BorderRadius = 10;
+            this.bunifuProgressBar1.BorderThickness = 1;
+            this.bunifuProgressBar1.Location = new System.Drawing.Point(74, 104);
+            this.bunifuProgressBar1.Maximum = 100;
+            this.bunifuProgressBar1.MaximumValue = 100;
+            this.bunifuProgressBar1.Minimum = 0;
+            this.bunifuProgressBar1.MinimumValue = 0;
+            this.bunifuProgressBar1.Name = "bunifuProgressBar1";
+            this.bunifuProgressBar1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.bunifuProgressBar1.ProgressBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(223)))), ((int)(((byte)(223)))));
+            this.bunifuProgressBar1.ProgressColorLeft = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(178)))), ((int)(((byte)(53)))));
+            this.bunifuProgressBar1.ProgressColorRight = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(0)))), ((int)(((byte)(11)))));
+            this.bunifuProgressBar1.Size = new System.Drawing.Size(384, 28);
+            this.bunifuProgressBar1.TabIndex = 0;
+            this.bunifuProgressBar1.Value = 50;
+            this.bunifuProgressBar1.ValueByTransition = 50;
+            // 
+            // bunifuLabel1
+            // 
+            this.bunifuLabel1.AllowParentOverrides = false;
+            this.bunifuLabel1.AutoEllipsis = false;
+            this.bunifuLabel1.CursorType = System.Windows.Forms.Cursors.Default;
+            this.bunifuLabel1.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.bunifuLabel1.Location = new System.Drawing.Point(145, 38);
+            this.bunifuLabel1.Name = "bunifuLabel1";
+            this.bunifuLabel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.bunifuLabel1.Size = new System.Drawing.Size(226, 32);
+            this.bunifuLabel1.TabIndex = 1;
+            this.bunifuLabel1.Text = "Generando planillas...";
+            this.bunifuLabel1.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.bunifuLabel1.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // FrmLoadPayroll
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 169);
+            this.Controls.Add(this.bunifuLabel1);
+            this.Controls.Add(this.bunifuProgressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmLoadPayroll";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmLoadPayroll";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.UI.WinForms.BunifuProgressBar bunifuProgressBar1;
+        private Bunifu.UI.WinForms.BunifuLabel bunifuLabel1;
+    }
+}
