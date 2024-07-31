441afb207f6c9ac8230a976f6fd486b129f69b7a
MaxJS07 <maxjimenezsep@gmail.com>
2024-07-30 18:10:40 -0600

    Se hizo la lectura de datos en el formulario FrmEmployees.

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Controller/EmployeesController/ControllerEmployees.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Controller/EmployeesController/ControllerEmployees.cs
@@ -1,6 +1,8 @@
-﻿using PTC2024.View.Empleados;
+﻿using PTC2024.Model.DAO.EmployeesDAO;
+using PTC2024.View.Empleados;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,16 +18,28 @@ namespace PTC2024.Controller.Employees
         {
             objEmployees = Vista;
             //Evento para cargar el dataGrid
-            objEmployees.Load += new EventHandler(LoadDataGridEmployees);
+            objEmployees.Load += new EventHandler(LoadData);
+            //Eventos de click de botones
             objEmployees.BtnAddEmployee.Click += new EventHandler(NewEmployee);
             objEmployees.cmsUpdateEmployee.Click += new EventHandler(UpdateEmployee);
-            //Eventos de click de botones
-            //objEmployees.BtnAgregarEmpleado.Click += new EventHandler(NuevoEmpleado);
+            objEmployees.cmsDeleteEmployee.Click += new EventHandler(DeleteEmployee);
         }
 
-        public void LoadDataGridEmployees(object sender, EventArgs e)
+        public void LoadData (object sender, EventArgs e)
         {
+            RefreshDataGridEmployees();
+        }
 
+        public void RefreshDataGridEmployees()
+        {
+            DAOEmployees daoEmployees = new DAOEmployees();
+            DataSet ds = daoEmployees.GetEmployees();
+            //Llenando el datagridview
+            objEmployees.dgvEmployees.DataSource = ds.Tables["viewEmployees"];
+            objEmployees.dgvEmployees.Columns[0].DividerWidth = 1;
+            objEmployees.dgvEmployees.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            objEmployees.dgvEmployees.Columns[4].Visible = false;
+            objEmployees.dgvEmployees.Columns[10].Visible = false;
         }
 
         public void NewEmployee(object sender, EventArgs e)
@@ -39,6 +53,11 @@ namespace PTC2024.Controller.Employees
         {
             FrmUpdateEmployee abrirfForm = new FrmUpdateEmployee();
             abrirfForm.ShowDialog();
+        }
+
+        public void DeleteEmployee(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/DAO/EmployeesDAO/DAOEmployees.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/DAO/EmployeesDAO/DAOEmployees.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PTC2024.Model.DAO.EmployeesDAO
 {
-    internal class DAOEmployees
+    internal class DAOEmployees : dbContext
     {
+        readonly SqlCommand Command = new SqlCommand();
+        
+        public DataSet GetEmployees()
+        {
+            try
+            {
+                Command.Connection = getConnection();
+                string query = "SELECT*FROM  viewEmployees";
+                SqlCommand cmd = new SqlCommand(query, Command.Connection);
+                cmd.ExecuteNonQuery();
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                DataSet dsEmployees = new DataSet();
+                adp.Fill(dsEmployees, "viewEmployees");
+                return dsEmployees;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudieron obtener los empleados existentes");
+                return null;
+              
+            }
+            finally
+            {
+                Command.Connection.Close();
+            }
+        }
     }
 }
