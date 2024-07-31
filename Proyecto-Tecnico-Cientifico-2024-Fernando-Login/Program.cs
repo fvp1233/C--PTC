3c52f4efd9bec62ba68b12acae10bf0bbddcbc65
Oscar Velasquez <oscar.joyar@gmail.com>
2024-07-31 16:43:54 -0600

    SE LOGROOOOOOOOOO

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Program.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Program.cs
@@ -23,7 +23,7 @@ namespace PTC2024
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmRegister ());
+            Application.Run(new StartMenu());
         }
 
     }
