611af088ec49d4335b972ede62e60925228550b5
Sherartsu <jose.merino9108@gmail.com>
2024-08-01 19:05:33 -0600

    Se agrego el delete y update de servicios

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/DTO/ServicesDTO/DTOServices.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/DTO/ServicesDTO/DTOServices.cs
@@ -8,5 +8,13 @@ namespace PTC2024.Model.DTO.ServicesDTO
 {
     internal class DTOServices : dbContext
     {
+        int IdService;
+
+        public int IdService1 { get => IdService; set => IdService = value; }
+
+
+        string Search;
+
+        public string Search1 { get => Search; set => Search = value; }
     }
 }
