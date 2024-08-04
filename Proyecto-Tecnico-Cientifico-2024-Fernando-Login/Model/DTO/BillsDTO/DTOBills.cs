74a4b5ef0051fae0da2b27de1a66091d76475915
Alfaro294 <jenniferalfaro0819@gmail.com>
2024-08-03 22:05:30 -0600

    cambios en controller addbills
    se realizaron cambios del controlador

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/DTO/BillsDTO/DTOBills.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/DTO/BillsDTO/DTOBills.cs
@@ -13,9 +13,9 @@ namespace PTC2024.Model.DTO.BillsDTO
         private string companyName;
         private string NIT;
         private string NRC;
-        private double discount;
-        private double subtotalPay;
-        private double totalPay;
+        private float discount;
+        private float subtotalPay;
+        private float totalPay;
         private DateTime startDate;
         private DateTime FinalDate;
         private string services;
@@ -27,14 +27,12 @@ namespace PTC2024.Model.DTO.BillsDTO
         //TABLE BILL DETAIL
         private int IdDetail;
         private int IdServices;
+        private float Price;
         //TABLE FISCAL PERIOD
         private int IdfiscalPeriod;
 
         public int IdBill1 { get => IdBill; set => IdBill = value; }
         public string CompanyName { get => companyName; set => companyName = value; }
-        public double Discount { get => discount; set => discount = value; }
-        public double SubtotalPay { get => subtotalPay; set => subtotalPay = value; }
-        public double TotalPay { get => totalPay; set => totalPay = value; }
         public DateTime StartDate { get => startDate; set => startDate = value; }
         public DateTime FinalDate1 { get => FinalDate; set => FinalDate = value; }
         public DateTime FiscalPeriod { get => fiscalPeriod; set => fiscalPeriod = value; }
@@ -48,5 +46,9 @@ namespace PTC2024.Model.DTO.BillsDTO
         public string Employee { get => employee; set => employee = value; }
         public string MethodP { get => methodP; set => methodP = value; }
         public int IdServices1 { get => IdServices; set => IdServices = value; }
+        public float Price1 { get => Price; set => Price = value; }
+        public float Discount { get => discount; set => discount = value; }
+        public float SubtotalPay { get => subtotalPay; set => subtotalPay = value; }
+        public float TotalPay { get => totalPay; set => totalPay = value; }
     }
 }
