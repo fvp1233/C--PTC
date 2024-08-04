1bfa68b78a58f8d8f34ca57c43d0f94bd4592143
MaxJS07 <maxjimenezsep@gmail.com>
2024-08-03 21:20:26 -0600

    se añadió la directiva using en el program para que sirva el método InitialView

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Program.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Program.cs
@@ -10,6 +10,7 @@ using PTC2024.View.formularios.inicio;
 using PTC2024.View.login;
 using PTC2024.View.InventarioServicios;
 using PTC2024.View.BillsViews;
+using PTC2024.Controller.Helper;
 using PTC2024.View.Clientes;
 using System.Web.UI.WebControls;
 
