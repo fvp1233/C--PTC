506b4b64ac0eceef3f9428696cffa2d3af7343be
Oscar Velasquez <oscar.joyar@gmail.com>
2024-08-18 22:28:33 -0600

    Se finalizaron todos los mantenimientos de permisos

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/DAO/PayrollsDAO/DAOPermission.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/DAO/PayrollsDAO/DAOPermission.cs
@@ -18,7 +18,7 @@ namespace PTC2024.Model.DAO.PayrollsDAO
             try
             {
                 Command.Connection = getConnection();
-                string query = "SELECT * FROM viewPermissions";
+                string query = "SELECT * FROM viewPermissions WHERE [Estado de permiso] = 'En proceso' OR  [Estado de permiso] = 'Completado'";
                 SqlCommand cmd = new SqlCommand(query, Command.Connection);
                 cmd.ExecuteNonQuery();
                 SqlDataAdapter adp = new SqlDataAdapter(cmd);
@@ -29,6 +29,74 @@ namespace PTC2024.Model.DAO.PayrollsDAO
             catch (SqlException ex)
             {
                 MessageBox.Show("EC-0100: No se puedieron obtener los datos de la tabla tbStatusPermission");
+                return null;
+            }
+            finally
+            {
+                Command.Connection.Close();
+            }
+        }
+        public int UpdateDsEPermission()
+        {
+            try
+            {
+                Command.Connection = getConnection();
+                string query = "UPDATE tbPermissions SET IdStatusPermission = @param1 WHERE IdEmployee = @param2";
+                SqlCommand cmd = new SqlCommand(query, Command.Connection);
+                cmd.Parameters.AddWithValue("@param1", IdStatusPermission);
+                cmd.Parameters.AddWithValue("@param2", IdEmployee);
+                int answer = cmd.ExecuteNonQuery();
+                return answer;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Excepcion{e}");
+                return -1;
+            }
+            finally
+            {
+                getConnection().Close();
+            }
+        }
+        public DataSet GetPermissionsDisable()
+        {
+            try
+            {
+                Command.Connection = getConnection();
+                string query = "SELECT * FROM tbPermissions";
+                SqlCommand cmd = new SqlCommand(query, Command.Connection);
+                cmd.ExecuteNonQuery();
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adp.Fill(ds, "tbPermissions");
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("EC-009: No se puedieron obtener los datos de la tabla tbStatusPayroll");
+                return null;
+            }
+            finally
+            {
+                Command.Connection.Close();
+            }
+        }
+        public DataSet GetEmployee()
+        {
+            try
+            {
+                Command.Connection = getConnection();
+                string query = "SELECT * FROM tbEmployee";
+                SqlCommand cmd = new SqlCommand(query, Command.Connection);
+                cmd.ExecuteNonQuery();
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adp.Fill(ds, "tbEmployee");
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("EC-009: No se puedieron obtener los datos de la tabla tbStatusPayroll");
                 return null;
             }
             finally
