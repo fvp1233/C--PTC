abe552e6f22ffd8919e90a8301f450b06e1cbfbb
fvp1233 <fernadomiguelvelasquezperez@gmail.com>
2024-09-03 22:41:26 -0600

    Conexion de base de datos en linea

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/dbContext.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Model/dbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -10,16 +11,29 @@ namespace PTC2024.Model
 {
     internal class dbContext
     {
+        private string server;
+        private string database;
+        private string user;
+        private string password;
+
+        public string Server { get => server; set => server = value; }
+        public string Database { get => database; set => database = value; }
+        public string User { get => user; set => user = value; }
+        public string Password { get => password; set => password = value; }
+
         public static SqlConnection getConnection()
         {
-			try
-			{
-                string server = "DESKTOP-L1S3JL8";
-                string database = "DBPTC_H2C24";
-                SqlConnection conexion = new SqlConnection($"Server = {server}; DataBase = {database}; Integrated Security = true");
+            try
+            {
+                string server = "SQL8005.site4now.net";
+                string database = "db_aaa7ca_ricaldone";
+                string userId = "db_aaa7ca_ricaldone_admin";
+                string Password = "Master2024";
+                SqlConnection conexion = new SqlConnection($"Server = {server}; DataBase = {database}; User Id = {userId}; Password = {Password}");
                 conexion.Open();
                 return conexion;
             }
+
 			catch (SqlException ex)
 			{
                 MessageBox.Show("Código de error: EC-001 \nNo fue posible conectarse a la base de datos, favor verifique las credenciales o que tenga acceso al sistema.", "Error crítico", MessageBoxButtons.OK, MessageBoxIcon.Error);
