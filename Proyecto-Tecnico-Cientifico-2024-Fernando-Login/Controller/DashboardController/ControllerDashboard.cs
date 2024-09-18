2960250db75466458635fa9c09c22ceee490254f
Oscar Abel <oscar.joyar@gmail.com>
2024-09-18 08:18:38 -0600

    Cambio UI Dashboard

--- Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Controller/DashboardController/ControllerDashboard.cs
+++ Proyecto-Tecnico-Cientifico-2024-Fernando-Login/Controller/DashboardController/ControllerDashboard.cs
@@ -17,10 +17,6 @@ namespace PTC2024.Controller.DashboardController
             objDashboard = View;
             objDashboard.Load += new EventHandler(ChargeValues);
             objDashboard.btnConfirm.Click += new EventHandler(DtpSearch);
-            objDashboard.btnFirstTrimester.Click += new EventHandler(FirstTrimester);
-            objDashboard.btnSecondTrimester.Click += new EventHandler(SecondTrimester);
-            objDashboard.btnThirdTrimester.Click += new EventHandler(ThirdTrimester);
-            objDashboard.btnFourthTrimester.Click += new EventHandler(FourthTrimester);
         }
         public void ChargeValues(object sender, EventArgs e)
         {
@@ -60,106 +56,6 @@ namespace PTC2024.Controller.DashboardController
         {
             DAODashboard dAODashboard = new DAODashboard();
             bool refreshData = dAODashboard.LoadData(objDashboard.dtpStart.Value, objDashboard.dtpEnd.Value);
-            if (refreshData == true)
-            {
-                dAODashboard.TotalPay = dAODashboard.GetTotalIncome();
-                objDashboard.lblTotalIncome.Text = $"${dAODashboard.TotalPay:N2}";
-
-                dAODashboard.GetAnalisys();
-                objDashboard.chartPayrolls.DataSource = dAODashboard.PayrollsList;
-                objDashboard.chartPayrolls.Series[0].XValueMember = "Date";
-                objDashboard.chartPayrolls.Series[0].YValueMembers = "TotalAmount";
-                objDashboard.chartPayrolls.DataBind();
-
-                dAODashboard.GetTopServices();
-                objDashboard.chrtTopServices.DataSource = dAODashboard.TopServices;
-                objDashboard.chrtTopServices.Series[0].XValueMember = "Key";
-                objDashboard.chrtTopServices.Series[0].YValueMembers = "Value";
-                objDashboard.chrtTopServices.DataBind();
-            }
-        }
-        public void FirstTrimester(object sender, EventArgs e)
-        {
-            DAODashboard dAODashboard = new DAODashboard();
-            int currentYear = DateTime.Now.Year;
-            DateTime startDate = new DateTime(currentYear, 1, 1);
-            DateTime endDate = new DateTime(currentYear, 3, 31);
-            bool refreshData = dAODashboard.LoadData(startDate, endDate);
-            if (refreshData == true)
-            {
-                dAODashboard.TotalPay = dAODashboard.GetTotalIncome();
-                objDashboard.lblTotalIncome.Text = $"${dAODashboard.TotalPay:N2}";
-
-                dAODashboard.GetAnalisys();
-                objDashboard.chartPayrolls.DataSource = dAODashboard.PayrollsList;
-                objDashboard.chartPayrolls.Series[0].XValueMember = "Date";
-                objDashboard.chartPayrolls.Series[0].YValueMembers = "TotalAmount";
-                objDashboard.chartPayrolls.DataBind();
-
-                dAODashboard.GetTopServices();
-                objDashboard.chrtTopServices.DataSource = dAODashboard.TopServices;
-                objDashboard.chrtTopServices.Series[0].XValueMember = "Key";
-                objDashboard.chrtTopServices.Series[0].YValueMembers = "Value";
-                objDashboard.chrtTopServices.DataBind();
-            }
-        }
-        public void SecondTrimester(object sender, EventArgs e)
-        {
-            DAODashboard dAODashboard = new DAODashboard();
-            int currentYear = DateTime.Now.Year;
-            DateTime startDate = new DateTime(currentYear, 4, 1);
-            DateTime endDate = new DateTime(currentYear, 6, 30);
-            bool refreshData = dAODashboard.LoadData(startDate, endDate);
-            if (refreshData == true)
-            {
-                dAODashboard.TotalPay = dAODashboard.GetTotalIncome();
-                objDashboard.lblTotalIncome.Text = $"${dAODashboard.TotalPay:N2}";
-
-                dAODashboard.GetAnalisys();
-                objDashboard.chartPayrolls.DataSource = dAODashboard.PayrollsList;
-                objDashboard.chartPayrolls.Series[0].XValueMember = "Date";
-                objDashboard.chartPayrolls.Series[0].YValueMembers = "TotalAmount";
-                objDashboard.chartPayrolls.DataBind();
-
-                dAODashboard.GetTopServices();
-                objDashboard.chrtTopServices.DataSource = dAODashboard.TopServices;
-                objDashboard.chrtTopServices.Series[0].XValueMember = "Key";
-                objDashboard.chrtTopServices.Series[0].YValueMembers = "Value";
-                objDashboard.chrtTopServices.DataBind();
-            }
-        }
-        public void ThirdTrimester(object sender, EventArgs e)
-        {
-            DAODashboard dAODashboard = new DAODashboard();
-            int currentYear = DateTime.Now.Year;
-            DateTime startDate = new DateTime(currentYear, 7, 1);
-            DateTime endDate = new DateTime(currentYear, 9, 30);
-            bool refreshData = dAODashboard.LoadData(startDate, endDate);
-            if (refreshData == true)
-            {
-                dAODashboard.TotalPay = dAODashboard.GetTotalIncome();
-                objDashboard.lblTotalIncome.Text = $"${dAODashboard.TotalPay:N2}";
-
-                dAODashboard.GetAnalisys();
-                objDashboard.chartPayrolls.DataSource = dAODashboard.PayrollsList;
-                objDashboard.chartPayrolls.Series[0].XValueMember = "Date";
-                objDashboard.chartPayrolls.Series[0].YValueMembers = "TotalAmount";
-                objDashboard.chartPayrolls.DataBind();
-
-                dAODashboard.GetTopServices();
-                objDashboard.chrtTopServices.DataSource = dAODashboard.TopServices;
-                objDashboard.chrtTopServices.Series[0].XValueMember = "Key";
-                objDashboard.chrtTopServices.Series[0].YValueMembers = "Value";
-                objDashboard.chrtTopServices.DataBind();
-            }
-        }
-        public void FourthTrimester(object sender, EventArgs e)
-        {
-            DAODashboard dAODashboard = new DAODashboard();
-            int currentYear = DateTime.Now.Year;
-            DateTime startDate = new DateTime(currentYear, 10, 1);
-            DateTime endDate = new DateTime(currentYear, 12, 31);
-            bool refreshData = dAODashboard.LoadData(startDate, endDate);
             if (refreshData == true)
             {
                 dAODashboard.TotalPay = dAODashboard.GetTotalIncome();
